616911ad6455b2c9f43a83b1b8edc990269bb9e7
chlacra <alexandra.chirieac@assist.ro>
2021-08-18 14:56:59 +0300

    modified

--- cypress/integration/cypress/integration/TCforAddToCart.js
+++
@@ -1,9 +0,0 @@
-// <reference types="cypress" />
-
-describe('Check  add an item to cart functionality', () => {
-    beforeEach(() => {
-  
-      cy.visit('http://live.demoguru99.com')
-    })
-
-    
---
+++ cypress/integration/trash.js
@@ -0,0 +1,55 @@
+cy.get('.product-cart-actions > .input-text')
+        .click()
+        .clear()
+        .type('2')
+        cy.get('.product-cart-actions > .button > :nth-child(1) > span').click()
+        cy.get('.nav-2 > .level0')
+        .click()
+
+
+       
+       //  cy.get('.action-select').should('have.value','billing[region_id]')//
+
+
+
+
+       it( 'Check  finalize a purchase', () => {
+        cy.get('body div.header-language-background p')
+        cy.get('.nav-1 > .level0').click()
+        cy.get('body > div > div > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul') 
+            .scrollIntoView() 
+        cy.get(':nth-child(1) > .product-info > .actions > .button > :nth-child(1) > span')
+            .click()
+        cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span')
+            .should('have.text', 'Samsung Galaxy was added to your shopping cart.')
+        cy.get(':nth-child(1) > .product-info > .actions > .button > :nth-child(1) > span').click()
+        cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span').should('have.text','LG LCD was added to your shopping cart.')
+        cy.get('.method-checkout-cart-methods-onepage-bottom > .button > :nth-child(1) > span').click()
+       // cy.get('.col-1 > .form-list > :nth-child(1) > label').click()
+       // cy.get('#onepage-guest-register-button > :nth-child(1) > span').click()
+    })
+
+CA SA EDITEZ CANTITATEA
+
+    cy.get('.product-cart-actions > .input-text')
+    .click()
+    .clear()
+    .type('2')
+    cy.get('.product-cart-actions > .button > :nth-child(1) > span').click()
+    cy.get('.nav-2 > .level0')
+    .click()
+    cy.get(':nth-child(1) > .product-info > .actions > .button > :nth-child(1) > span').click()
+    cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span').should('have.text','LG LCD was added to your shopping cart.')
+
+    it.only( 'Check add items to cart functionality from ShoppingCart', () => {
+        cy.get('body div.header-language-background p')
+        cy.get('#search')
+            .click()
+            .type('Samsung {enter}')
+        cy.get(':nth-child(1) > .product-info > .actions > .button > :nth-child(1) > span').click()
+        cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span').should('have.text','Samsung LCD was added to your shopping cart.')
+       
+
+
+        
+    })
